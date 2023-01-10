f9821849a4b23f13ec6eb577a23e6478d3cb28ac
Ingrid Delteil <ingrid.delteil@gmail.com>
2023-01-10 20:59:05 +0100

    Ajout du composant Oups pour gérer les erreurs 404

--- src/pages/Oups.js
+++ src/pages/Oups.js
@@ -1,7 +1,18 @@
 import React from "react";
+import MainContainer from "../components/MainContainer";
+import Title from "../components/Title";
+import {Stack} from "@mui/material";
+import perdu from "../assets/perdu.svg";
 
 const Oups = () => {
-  return <div>Oups</div>;
+  return (
+    <MainContainer>
+      <Stack alignItems='center' spacing={2}>
+        <img src={perdu} alt='Perdu' style={{maxWidth: "300px"}} />
+        <Title>Oups ! Essaie un autre chemin ...</Title>
+      </Stack>
+    </MainContainer>
+  );
 };
 
 export default Oups;
